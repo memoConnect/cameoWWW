baeaba64a242556e59f6d09786f70bc36ebbe65d
reimerei <github@bjrm.de>
2014-05-21 17:31:53 +0200

    added redirect

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,6 +4,10 @@ import play.api.mvc._
 
 object Application extends Controller {
 
+  def redirect(url: String) = Action {
+    Redirect(url)
+  }
+
   def index = Action {
     //val lang: String = "de"
     Ok(views.html.index())

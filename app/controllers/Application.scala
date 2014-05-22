ca580bac14d1c452e183daa9f70199c6a79f11dd
Empujador <b.schaedel@xuffix.com>
2014-05-22 09:35:22 +0200

    add honeypot and hidden input to form update message_de set html_entitites

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,6 +4,7 @@ import play.api.mvc._
 import _root_.helper.i18n
 
 import models.Antibot
+import play.Logger
 
 object Application extends Controller {
   def redirect(url: String) = Action {
@@ -15,9 +16,9 @@ object Application extends Controller {
 
     i18n.setLang(lang)
 
-    val ab: new Antibot("moep", "moep-value")
-    Logger.debug(ab.name)
+    val ab = new Antibot("moep", "moep-value")
+//    Logger.debug(ab.name)
 
-    Ok(views.html.index())
+    Ok(views.html.index(ab))
   }
 }

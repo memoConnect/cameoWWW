5eabd5cab67fbf3661c56ea961315d361c1dc2fd
Empujador <b.schaedel@xuffix.com>
2014-05-23 14:15:43 +0200

    clear shit

--- app/models/Antibot.scala
+++ app/models/Antibot.scala
@@ -4,8 +4,8 @@ package models
  * Created by Empujador on 21.05.2014.
  */
 class Antibot(name: String, value: String){
-  val name: String
-  val value: String
+//  val name: String
+//  val value: String
 
 //  def create (name: String, value: String) {
 //    name = "name-moep"

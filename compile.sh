e45eb27439c3489aa9c7240a646e4ba737934bf1
reimerei <github@bjrm.de>
2014-05-26 15:29:52 +0200

    copy secret to right location

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ ${1} == "--with-secrets" ]]; then
-    cp ../cameoSecrets/www_secret.conf conf/secret.conf
-else
-    echo "include \"application\"" > conf/secret.conf
-fi
-
 ./sbt clean compile stage
+
+if [[ ${1} == "--with-secrets" ]]; then
+    cp ../cameoSecrets/www_secret.conf target/universal/stage/conf/secret.conf
+else
+    echo "include \"application\"" > target/universal/stage/conf/secret.conf
+fi

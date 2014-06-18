6fffd316d6242be8201d16c8a7cc0030384af56b
reimerei <github@bjrm.de>
2014-06-18 13:00:44 +0200

    contact formular backend: converted form submit to ajax

--- app/controllers/FormSubmit.scala
+++ app/controllers/FormSubmit.scala
@@ -1,68 +1,66 @@
 package controllers
 
-import play.api.mvc._
-import play.api.data.Forms._
-import play.api.data.Form
 import models.Antibot
-import play.api.Play.current
 import org.apache.commons.mail.EmailException
 import play.api.Logger
+import play.api.Play.current
+import play.api.libs.json.{JsError, JsObject, Json}
+import play.api.mvc._
 
 object FormSubmit extends Controller {
 
-  case class MessageForm(name: String,
-                          email: String,
-                          subject: String,
-                          message: String)
+  case class MessageSubmit(name: String,
+                           email: String,
+                           subject: String,
+                           message: String,
+                           abName: String,
+                           abValue: String,
+                           additionalFields: Option[JsObject])
 
-  val messageForm = Form(
-    mapping(
-      "name" -> nonEmptyText,
-      "email" -> nonEmptyText,
-      "subject" -> text,
-      "message" -> text
-    )(MessageForm.apply)(MessageForm.unapply)
-  )
-
-  def submit = Action {
-
-    implicit request =>
-      messageForm.bindFromRequest.fold(
-        error =>
-          BadRequest(error.errorsAsJson),
-        messageForm =>
-          validate(request, messageForm)
-      )
+  object MessageSubmit {
+    implicit val format = Json.format[MessageSubmit]
   }
 
-  def validate(request: Request[AnyContent], messageForm: MessageForm) : Result = {
-    val ab = Antibot.create(request)
+  def submit = Action(parse.tolerantJson) {
+    request =>
+      request.body.validate[MessageSubmit].map {
+        ms =>
+          Logger.info("Received contact request: " + request.body)
+          // validate antibot
+          Antibot.create(request).equals(new Antibot(ms.abName, ms.abValue)) match {
+            case false => BadRequest("Invalid antibot values")
+            case true => sendMail(ms)
+          }
 
-    request.body.asFormUrlEncoded.get.find(
-      _._1.equals(ab.name)
-    ) match {
-      case None => BadRequest("key not found")
-      case Some (values) => values._2.headOption match {
-        case None => BadRequest("value not found")
-        case Some (value) => value.equals(ab.value) match {
-          case false => BadRequest("invalid value")
-          case true => sendMail(messageForm)
-        }
+      }.recoverTotal {
+        err => BadRequest(JsError.toFlatJson(err))
       }
-    }
   }
 
-  def sendMail(messageForm: MessageForm): Result = {
+
+  def sendMail(messageSubmit: MessageSubmit): Result = {
     import com.typesafe.plugin._
 
     val mail = use[MailerPlugin].email
-    mail.setSubject("[Kontaktformular] " + messageForm.subject)
-    mail.setRecipient("Support <support@cameo.io>")
+    mail.setSubject("[Kontaktformular] " + messageSubmit.subject)
+    mail.setRecipient("Support <support@cameo.io>") //todo: put this in config
     mail.setFrom("noreply@cameo.io")
-    mail.setReplyTo(messageForm.name + " <"+ messageForm.email + ">")
+    mail.setReplyTo(messageSubmit.name + " <" + messageSubmit.email + ">")
+    
+    // append additional values
+    val maybeAppend = messageSubmit.additionalFields.map{
+      af => af.keys.map {
+        key => key + ":" + (af \ key).as[String]
+      }.mkString("\n")
+    }
+
+    val messageBody = maybeAppend match {
+      case None => messageSubmit.message
+      case Some(text) => messageSubmit.message + "\n\nAdditional Values:\n"+ text
+    }
 
     try {
-      mail.send(messageForm.message)
+      mail.send(messageBody)
     } catch {
       case ee: EmailException => Logger.error("Error sending mail: ", ee)
     }

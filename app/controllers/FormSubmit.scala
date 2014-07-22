02f0a36af595f1cab98734f645ae7ee4f8d46ac7
reimerei <github@bjrm.de>
2014-07-22 15:35:52 +0200

    added call to liveagent api

--- app/controllers/FormSubmit.scala
+++ app/controllers/FormSubmit.scala
@@ -2,10 +2,14 @@ package controllers
 
 import models.Antibot
 import org.apache.commons.mail.EmailException
-import play.api.Logger
 import play.api.Play.current
 import play.api.libs.json.{JsError, JsObject, Json}
+import play.api.libs.ws.WS
 import play.api.mvc._
+import play.api.{Logger, Play}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 object FormSubmit extends Controller {
 
@@ -21,22 +25,87 @@ object FormSubmit extends Controller {
     implicit val format = Json.format[MessageSubmit]
   }
 
-  def submit = Action(parse.tolerantJson) {
+  def submit = Action.async(parse.tolerantJson) {
     request =>
       request.body.validate[MessageSubmit].map {
         ms =>
           Logger.info("Received contact request: " + request.body)
           // validate antibot
           Antibot.create(request).equals(new Antibot(ms.abName, ms.abValue)) match {
-            case false => BadRequest("Invalid antibot values")
-            case true => sendMail(ms)
+            case false => Future(BadRequest("Invalid antibot values"))
+            case true =>
+              createTicket(ms)
+            //sendMail(ms)
           }
 
       }.recoverTotal {
-        err => BadRequest(JsError.toFlatJson(err))
+        err => Future(BadRequest(JsError.toFlatJson(err)))
       }
   }
 
+  def createTicket(messageSubmit: MessageSubmit): Future[Result] = {
+
+    // append additional values
+    val maybeAppend = messageSubmit.additionalFields.map {
+      af => af.keys.map {
+        key => key + ":" + (af \ key).as[String]
+      }.mkString("\n")
+    }
+
+    val messageBody = maybeAppend match {
+      case None => messageSubmit.message
+      case Some(text) => messageSubmit.message + "\n\nAdditional Values:\n" + text
+    }
+
+    val body =
+      Seq(
+        "message" -> messageBody,
+        "useridentifier" -> "support@cameo.io",
+        "department" -> "default",
+        "subject" -> messageSubmit.subject,
+        "recipient" -> messageSubmit.email,
+        "recipient_name" -> messageSubmit.name,
+        "apikey" -> Play.configuration.getString("liveagent.apikey").get
+      )
+
+//    val jsonBody =
+//      Json.obj(
+//        "message" -> messageBody,
+//        "useridentifier" -> "support@cameo.io",
+//        "department" -> "default",
+//        "subject" -> messageSubmit.subject,
+//        "recipient" -> messageSubmit.email,
+//        "recipient_name" -> messageSubmit.name,
+//        "apikey" -> Play.configuration.getString("liveagent.apikey").get
+//      )
+
+//    val body =
+//      Map(
+//        "message" -> "foo",
+//        "useridentifier" -> "support",
+//        "department" -> "default",
+//        "subject" -> "subj",
+//        "recipient" -> "foo",
+//        "recipient_name" -> "name",
+//        "apikey" -> Play.configuration.getString("liveagent.apikey").get
+//      )
+
+    val bodyString = body.map(map => map._1 + "=" + map._2).mkString("&")
+
+    Logger.debug("body:" + bodyString + ":")
+
+    val url = Play.configuration.getString("liveagent.url").get + "/api/conversations"
+
+    val response = WS.url(url).withHeaders(("content-type","application/x-www-form-urlencoded;charset=UTF-8")).post(bodyString)
+
+    response.map {
+      response =>
+        Logger.debug(response.body)
+        Ok("mail send")
+    }
+
+
+  }
 
   def sendMail(messageSubmit: MessageSubmit): Result = {
     import com.typesafe.plugin._
@@ -46,9 +115,9 @@ object FormSubmit extends Controller {
     mail.setRecipient("Support <support@cameo.io>") //todo: put this in config
     mail.setFrom("noreply@cameo.io")
     mail.setReplyTo(messageSubmit.name + " <" + messageSubmit.email + ">")
-    
+
     // append additional values
-    val maybeAppend = messageSubmit.additionalFields.map{
+    val maybeAppend = messageSubmit.additionalFields.map {
       af => af.keys.map {
         key => key + ":" + (af \ key).as[String]
       }.mkString("\n")
@@ -56,7 +125,7 @@ object FormSubmit extends Controller {
 
     val messageBody = maybeAppend match {
       case None => messageSubmit.message
-      case Some(text) => messageSubmit.message + messageSubmit.name + ", " + messageSubmit.email + "\n\nAdditional Values:\n"+ text
+      case Some(text) => messageSubmit.message + messageSubmit.name + ", " + messageSubmit.email + "\n\nAdditional Values:\n" + text
     }
 
     try {

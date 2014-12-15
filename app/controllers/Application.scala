a377bb8749d99dbf1627da204399c3d3387482a8
Michael Merz <dermicha@cameo.io>
2014-12-15 18:21:25 +0100

    en now default, de just for german people

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import play.api.Play
 import play.api.mvc._
 import _root_.helper.i18n
 
@@ -10,26 +11,30 @@ import play.modules.statsd.Statsd
 object Application extends Controller {
 
   def tryToTrack() = Action {
+
     implicit request =>
+
       request.getQueryString("qr") match {
         case Some(qrId) => Statsd.increment("custom.qrcodes.code-" + qrId)
           Logger.debug("QRCode tracked: " + qrId)
         case None =>
       }
-      Redirect("/w/de")
+
+      request.acceptLanguages.headOption match {
+        case Some(lang) => lang.language match {
+          case "de" => Redirect("/w/de")
+          case _ => Redirect("/w/en")
+        }
+        case None => Redirect("/w/en")
+      }
   }
 
   def redirect(url: String) = Action {
     Redirect(url)
   }
 
-  def index(lang: String = "de") = Action { request =>
-    // set lang
-
-
-
+  def index(lang: String = "en") = Action { request =>
     i18n.setLang(lang)
-
     Ok(views.html.index(Antibot.create(request)))
   }
 }
